1db3ad1ed8c0d72b6fd548546ee96a8b0f77674b
AISimonetta <info@simonettaalejandra.com>
2024-02-12 16:10:24 +0000

    setting up structure to later develop

--- api/src/main/java/com/nology/api/Service.java
+++ api/src/main/java/com/nology/api/Service.java
@@ -1,4 +1,67 @@
 package com.nology.api;
 
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+//logic
+@Service
 public class Service {
+//    @Autowired
+//    GreetingsRepository greetingsRepository;
+//
+//    // CREATE
+//
+//    public void addGreeting(Greeting greeting) {
+//        greetingsRepository.save(greeting);
+//    }
+//
+//
+//    // READ
+//
+//    public Greeting getGreetingById(long id) {
+//        Optional<Greeting> greeting = greetingsRepository.findById(id);
+//
+//        if (greeting.isEmpty()) {
+//            throw new GreetingNotFoundException();
+//        }
+//
+//        return greeting.get();
+//    }
+//
+//    public Greeting getRandomGreeting() {
+//        return greetingsRepository.getRandomGreeting();
+//    }
+//
+//    public List<Long> getGreetingIds() {
+//        return greetingsRepository.getDistinctIds();
+//    }
+//
+//    public List<String> getOriginCountries() {
+//        return greetingsRepository.getDistinctOriginCountries();
+//    }
+/
+//
+//    // UPDATE
+//
+//    public void updateGreeting(Greeting newGreeting, long id) {
+//        if (!greetingsRepository.existsById(id)) {
+//            throw new GreetingNotFoundException();
+//        }
+//
+//        newGreeting.setId(id);
+//
+//        greetingsRepository.save(newGreeting);
+//    }
+//
+//    // DELETE
+//    @Transactional
+//    public void deleteGreetingById(long id) {
+//        if (!greetingsRepository.existsById(id)) {
+//            throw new GreetingNotFoundException();
+//        }
+//
+//        greetingsRepository.deleteGreetingById(id);
+//    }
 }
